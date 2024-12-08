0479686a73ea380c57c64c8e09e9a4225b7c4a39
kksat <22549266+kksat@users.noreply.github.com>
2024-12-08 11:03:02 +0100

    set tmux for wezterm only if running on macos
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- dot-wezterm.lua
+++ dot-wezterm.lua
@@ -2,6 +2,9 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'Fira Code'
-config.default_prog = { "/opt/homebrew/bin/tmux" }
+
+if wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin" then
+  config.default_prog = { "/opt/homebrew/bin/tmux" }
+end
 
 return config
