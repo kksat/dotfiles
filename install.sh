f5d554aa63a01d0fe598bede735956914bf4115e
kksat <22549266+kksat@users.noreply.github.com>
2024-12-09 11:43:12 +0100

    add brew to bashrc profile
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 sudo apt-get install build-essential
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> ~/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 brew install stow
 
 make install
