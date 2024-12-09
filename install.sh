3484fcb92b5aa8b2737f6970de498231657f62ad
kksat <22549266+kksat@users.noreply.github.com>
2024-12-09 11:42:51 +0100

    add brew dependencies
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+sudo apt-get install build-essential
+
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install stow
 
