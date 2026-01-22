fd7a9ff2c7ee4790d6d8bf83558fa2586c2dbeb2
Kirill Satarin <22549266+kksat@users.noreply.github.com>
2026-01-22 16:45:30 +0100

    add pipefail
    Signed-off-by: Kirill Satarin <22549266+kksat@users.noreply.github.com>

--- dot.sh
+++ dot.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
+set -euo pipefail
+
 cd ~
 git clone https://github.com/kksat/dot.git
--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -euo pipefail
 
 sudo apt-get install build-essential
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo >> ~/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+
+# shellcheck disable=SC2016
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc  
+
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew install stow
